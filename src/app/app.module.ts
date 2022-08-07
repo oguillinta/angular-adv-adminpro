a98ed5bcd624cb2781520e70540588dd5ff88a79
oguillinta <oscar.guillinta93@gmail.com>
2022-08-07 16:09:54 -0500

    End of section 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraficalComponent } from './pages/grafical/grafical.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    BreadcrumbComponent,
+    SidebarComponent,
+    HeaderComponent,
+    RegisterComponent,
+    ProgressComponent,
+    GraficalComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/grafical/grafical.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grafical',
+  templateUrl: './grafical.component.html',
+  styleUrls: ['./grafical.component.css']
+})
+export class GraficalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
